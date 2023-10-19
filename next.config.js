958ace447a2ac97d4970331b3c3a1bea76d89503
Nick Vigilante <nickvigilante@users.noreply.github.com>
2023-10-19 01:38:56 -0400

    Fix gitignore (#3)
    * fix .gitignore
    
    * disable image optimization

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const withNextra = require('nextra')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = withNextra(nextConfig)
