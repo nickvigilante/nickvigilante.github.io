ed47f14aac6eb6adb77b8aacf9fda39b721b5c8a
Nick Vigilante <github@vigiemail.com>
2023-10-19 01:21:38 -0400

    Use Nextra website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig)
