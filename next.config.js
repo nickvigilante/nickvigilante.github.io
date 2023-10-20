92dfc2dce004bb5bcd802982f056b0cce2c0cc7e
nickvigilante <util+github@vigi.email>
2023-10-20 10:53:22 -0400

    Terminal-based website plus preview URLs

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.js',
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-}
+const nextConfig = {}
 
-module.exports = withNextra(nextConfig)
+module.exports = nextConfig
